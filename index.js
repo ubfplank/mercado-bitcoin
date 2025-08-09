9cadac7cac38daf2433653c2591a86d6b7285e13
ghopb <ghopb1108@gmail.com>
2025-08-09 14:22:20 -0300

    initialize WebSocket connection for market monitoring

---
+++ index.js
@@ -0,0 +1,32 @@
+// ETAPA 1
+//MONITORAR O MERCADO
+
+//ETAPA 2 
+//AUTENTICAR NA API'
+
+// ETAPA 3
+//ENVIAR ORDENS
+const WebSocket = require("ws");
+
+const ws = new WebSocket("wss://ws.mercadobitcoin.net/ws");
+
+ws.onopen = () => {
+    ws.send(JSON.stringify({
+        type: "subscribe",
+        subscription: {
+            name: "ticker",
+            id: "BRLBTC"
+        }
+    }));
+
+    ws.onmessage = (evt) => {
+        console.clear();
+        console.log(evt.data);
+    };
+};
+
+//ETAPA 2
+//AUTENTICAR NA API
+
+//ETAPA 3
+//ENVIAR ORDENS
