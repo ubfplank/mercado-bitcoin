dd8fb6a7263dc9cd02877e588766c84bfe09bea6
ghopb <ghopb1108@gmail.com>
2025-08-11 19:07:36 -0300

    add .env configuration and enhance API authentication flow

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-// ETAPA 1
-//MONITORAR O MERCADO
-
-//ETAPA 2 
-//AUTENTICAR NA API'
-
-// ETAPA 3
-//ENVIAR ORDENS
+require("dotenv").config();
+const axios = require("axios");
 const WebSocket = require("ws");
 
+let access_token = null;
+
+login()
+
+// ETAPA 1
+// MONITORAR O MERCADO
+
+// ETAPA 2 
+// AUTENTICAR NA API
+async function login() {
+    const url = 'https://api.mercadobitcoin.net/api/v4/authorize';
+    const body = { login: process.env.API_KEY, password: process.env.API_SECRET };
+
+    const { data } = await axios.post(url, body);
+    console.log("Acesso autorizado !");
+    setTimeout(login, (data.expiration * 1000) - Date.now());
+    access_token = data.access_token;
+}
+
+async function getAccountId() {
+    const url = 'https://api.mercadobitcoin.net/api/v4/account';
+    const headers = { Authorization: `Bearer ${access_token}` };
+    const { data } = await axios.get(url, { headers });
+
+    console.log(data);
+    process.exit(0);
+}
+
+// ETAPA 3
+// ENVIAR ORDENS
 const ws = new WebSocket("wss://ws.mercadobitcoin.net/ws");
 
 ws.onopen = () => {
@@ -15,7 +39,7 @@ ws.onopen = () => {
         type: "subscribe",
         subscription: {
             name: "ticker",
-            id: "BRLBTC"
+            id: "" + process.env.STREAM_ID
         }
     }));
 
@@ -24,9 +48,24 @@ ws.onopen = () => {
         console.log(evt.data);
     };
 };
+//side= buy or sell
+async function newOrder(side    
+    const url = `https://api.mercadobitcoin.net/api/v4/accounts/${process.env.ACCOUNT_ID}/${process.env.symbol}/orders`;
+    const body = {
+        qty:process.env.BUY_QTY,
+        side,
+        type:"market"
 
-//ETAPA 2
-//AUTENTICAR NA API
-
-//ETAPA 3
-//ENVIAR ORDENS
+    }
+const headers = { Authorization: `Bearer ${access_token}` };
+  try{
+const { data } = await axios.post(url, body, { headers });
+    console.log(data);  
+  }
+  catch(err){
+    console.error(err.response.? err.response.data : err.message);
+    process.exit(0);
+    
+  )
+    }    
+}
